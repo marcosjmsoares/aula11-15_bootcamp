7f039d72ecb09c13a6b92410f332c72876fa37dd
Marcos Soares <marcosjmsoares@gmail.com>
2025-09-21 20:36:50 -0300

    ajustes finais

--- usar_csv.py
+++ usar_csv.py
@@ -1,13 +1,21 @@
-from .interface.classes.csv_class import CsvProcessor
-#import pandas as pd
 
+from src.interface.classes.csv_class import CsvProcessor
+# import pandas as pd
 
-caminho_csv = './exemplo.csv'
+arquivo_csv = './exemplo.csv'
 filtro = 'estado'
 limite = 'SP'
 
-arquivo_csv = CsvProcessor(caminho_csv)
-arquivo_csv.carregar_csv()
-print(arquivo_csv.filtrar_por(filtro,limite))
-print(arquivo_csv.sub_filtro('preco','10,50'))
-print(arquivo_csv.df)
+arquivo_CSV = CsvProcessor(arquivo_csv)
+arquivo_CSV.carregar_csv()  
+print(arquivo_CSV.filtrar_por(['estado', 'preco'], ['SP', '10,50']))
+# print(arquivo_CSV.df)
+print("--------------------------")
+# arquivo_csv2 = './examplo2.csv'
+# filtro2 = 'estado'  # Changed to filtro2
+# limite2 = 'DF'
+
+# arquivo_CSV2 = CsvProcessor(arquivo_csv2)
+# arquivo_CSV2.carregar_csv()  # Load the CSV
+# print(arquivo_CSV2.filtrar_por(filtro2, limite2))  # Changed to filtro2
+# print(arquivo_CSV2.sub_filtro('preço', '10,50'))
